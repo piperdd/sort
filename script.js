f1bc556791430e847a89b2f3d4608c7bbd24de59
piperdd <harunazh2003@gmail.com>
2023-12-31 15:18:23 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// import Sortable from 'sortablejs';
+
+Sortable.create(list, {
+    animation: 150,
+    // sort: true
+    ghostClass: 'ghost'
+});
+
+// new Sortable(list, {
+//     group: "sorting",
+//     sort: true
+// });
